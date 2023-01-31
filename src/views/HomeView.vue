9337d38033b7ab86b59c50a0853e7d9b97c0687b
Alexandros Stergiou <jutaroroche@gmail.com>
2023-01-31 21:27:44 +0100

    Added axios, test request to API

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,26 @@
-<script setup lang="ts">
-import HelloWorld from "@/components/HelloWorld.vue";
-</script>
-
 <template>
   <HelloWorld msg="Hello from HomeView.vue" />
+  <button class="btn btn-accent" @click="testRequest">TEST</button>
 </template>
+
+<script setup lang="ts">
+import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
+
+axios.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
+const testRequest = () => {
+  const params = new URLSearchParams({
+    page: "0",
+    size: "10",
+  });
+
+  axios
+    .get(`http://localhost:8080/api/player`, {
+      params: params,
+      /*headers: {
+              "Access-Control-Allow-Origin": "*",
+            },*/
+    })
+    .then((data) => console.log(data));
+};
+</script>
