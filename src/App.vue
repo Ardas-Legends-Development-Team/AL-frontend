ba22556fd9ce1fa85f1ba4bf3da5ca40c699e665
Alexandros Stergiou <jutaroroche@gmail.com>
2023-02-01 21:18:14 +0100

    Added footer and page icon. Added min height of main content to fill screen

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,7 @@
-<script>
-import { defineComponent } from "vue";
+<script setup lang="ts">
 import TestNavbar from "@/components/navbars/HorizontalNavbar.vue";
 import VerticalNavbar from "@/components/navbars/VerticalNavbar.vue";
-
-export default defineComponent({
-  components: { VerticalNavbar, TestNavbar },
-});
+import FooterBar from "@/components/navbars/FooterBar.vue";
 </script>
 
 <template>
@@ -13,10 +9,8 @@ export default defineComponent({
     <TestNavbar />
     <VerticalNavbar />
   </header>
-  <body>
-    <div class="static py-4 z-0 mx-60 bg-base-300">
-      <router-view />
-    </div>
-  </body>
-  <footer></footer>
+  <main class="relative min-h-screen py-12 z-0 mx-60 bg-base-300">
+    <router-view />
+  </main>
+  <FooterBar />
 </template>
--- src/views/ListsView.vue
+++ src/views/ListsView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="min-height-full">
     <RoleplayDataCard />
   </div>
 </template>
