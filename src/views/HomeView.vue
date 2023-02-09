df32081e0ba54ddeb5a4e4b012b3d4f62bef688b
Alexandros Stergiou <jutaroroche@gmail.com>
2023-02-09 18:41:49 +0100

    Added homepage hero

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,26 +1,7 @@
 <template>
-  <HelloWorld msg="Hello from HomeView.vue" />
-  <button class="btn btn-accent" @click="testRequest">TEST</button>
+  <HomeHero />
 </template>
 
 <script setup lang="ts">
-import HelloWorld from "@/components/HelloWorld.vue";
-import axios from "axios";
-
-axios.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
-const testRequest = () => {
-  const params = new URLSearchParams({
-    page: "0",
-    size: "10",
-  });
-
-  axios
-    .get(`http://localhost:8080/api/player`, {
-      params: params,
-      /*headers: {
-              "Access-Control-Allow-Origin": "*",
-            },*/
-    })
-    .then((data) => console.log(data));
-};
+import HomeHero from "@/components/HomeHero.vue";
 </script>
