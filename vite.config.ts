dce5dc9bf04a7540130f8c1d66ab7bdd77a1ec52
Alexandros Stergiou <jutaroroche@gmail.com>
2023-04-19 18:21:29 +0200

    First upgrade phase: add e2e and unit tests, add github CI, docker and auto-import libs

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,32 @@
 import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    // eslint-disable-next-line new-cap
+    AutoImport({
+      imports: ["vue", "vue-router"],
+      dts: "src/auto-imports.d.ts",
+      vueTemplate: true,
+    }),
+    // eslint-disable-next-line new-cap
+    Components({
+      dts: "src/components.d.ts",
+      extensions: ["vue"],
+      include: [/\.vue$/, /\.vue\?vue/],
+    }),
+  ],
   test: {
-    globals: true,
+    include: ["test/**/*.test.ts"],
     environment: "jsdom",
+    deps: {
+      inline: ["@vue", "@vueuse"],
+    },
   },
   resolve: {
     alias: {
