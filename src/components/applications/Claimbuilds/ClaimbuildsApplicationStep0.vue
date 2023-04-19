d8e9bf65a681cc6c6d2464dcf44f0bcbf0b2112f
Alexandros Stergiou <jutaroroche@gmail.com>
2023-04-19 19:48:57 +0200

    Fixed bad html tag warning

--- src/components/applications/Claimbuilds/ClaimbuildsApplicationStep0.vue
+++ src/components/applications/Claimbuilds/ClaimbuildsApplicationStep0.vue
@@ -1,39 +1,37 @@
 <template>
   <div>
-    <label class="label">
-      <div class="label-text text-secondary">
-        <b>Notes:</b>
+    <div class="label label-text text-secondary">
+      <ul>
+        <li><b>Notes:</b></li>
 
-        <ul>
-          <li>
-            <br />
-            You can lose your builds in wars if you submit for it to be a
-            claimbuild.
-          </li>
-          <li>
-            <br />
-            If a faction has no leader, the monthly resources for a build are
-            claimable by each player for their build. If a faction does have a
-            leader, it is up to the faction leader to split the resources among
-            faction members.
-          </li>
-          <li>
-            <br />
-            We strive to review claimbuild applications within 48 hours.
-            Unfortunately, we're not always able to meet this goal due to
-            personal and professional lives. If you notice that your application
-            does not have any votes after 48 hours, please tag an admin.
-          </li>
-          <li>
-            <br />
-            A positive number of flamingos after 48 hours OR a majority of admin
-            votes means your application is approved. A thread will be made by
-            an admin outlining what needs to change in your build if it has not
-            been accepted.
-          </li>
-        </ul>
-      </div>
-    </label>
+        <li>
+          <br />
+          You can lose your builds in wars if you submit for it to be a
+          claimbuild.
+        </li>
+        <li>
+          <br />
+          If a faction has no leader, the monthly resources for a build are
+          claimable by each player for their build. If a faction does have a
+          leader, it is up to the faction leader to split the resources among
+          faction members.
+        </li>
+        <li>
+          <br />
+          We strive to review claimbuild applications within 48 hours.
+          Unfortunately, we're not always able to meet this goal due to personal
+          and professional lives. If you notice that your application does not
+          have any votes after 48 hours, please tag an admin.
+        </li>
+        <li>
+          <br />
+          A positive number of flamingos after 48 hours OR a majority of admin
+          votes means your application is approved. A thread will be made by an
+          admin outlining what needs to change in your build if it has not been
+          accepted.
+        </li>
+      </ul>
+    </div>
   </div>
 
   <button class="btn btn-outline w-full" @click="nextStep()">OK!</button>
