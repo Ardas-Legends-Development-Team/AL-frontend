c3d7f766ca3d2d86fc45a32403efe3f9ab25ca8d
Luktronic <luka.furundzija@gmx.at>
2023-07-15 18:39:31 +0200

    Fixed backendUrl not being called correctly

--- src/ts/ApiService/ApiClient.ts
+++ src/ts/ApiService/ApiClient.ts
@@ -1,7 +1,7 @@
 import { backendUrl } from "@/config.json";
 
 export abstract class ApiClient {
-  private static baseUrl: string = `http://${backendUrl}/api`;
+  private static baseUrl: string = `http://${backendUrl.production}/api`;
 
   public static getBaseUrl(): string {
     return this.baseUrl;
