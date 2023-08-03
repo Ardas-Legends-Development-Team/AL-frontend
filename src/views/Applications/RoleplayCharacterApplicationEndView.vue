0ddc8437fcccb49728fa40ee57ff392167074896
Alexandros Stergiou <jutaroroche@gmail.com>
2023-08-03 18:45:38 +0200

    Removed duplicate image for RP app end view

--- src/views/Applications/RoleplayCharacterApplicationEndView.vue
+++ src/views/Applications/RoleplayCharacterApplicationEndView.vue
@@ -9,11 +9,6 @@
         :good-src="'https://ateettea.sirv.com/Applications/Roleplay/Dark-Eyes-Open_portrait_of_Aragon_Legolas_and_Gimli_enjoying_yu_30dd9010-e5c4-4d7d-a938-d22a83cb70b6.png'"
         :is-evil="isFactionEvil(usePlayerStore().faction)"
       />
-      <img
-        alt="elf army with general"
-        src="https://cdn.discordapp.com/attachments/1068863871772790865/1068869733966942258/VernonRoche__CA9.io_lord_of_the_rings_painting_of_an_elven_army_a48b17a6-23f0-4f03-8af6-07282d3b1aa1.png"
-        class="max-w-sm rounded-lg shadow-2xl"
-      />
       <div>
         <h1 class="text-5xl font-bold">Character Submitted!</h1>
         <p class="py-6">
