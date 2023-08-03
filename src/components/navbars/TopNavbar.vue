668ca8dbc8528e3cfaaf6d8d770444e1ba286753
Alexandros Stergiou <jutaroroche@gmail.com>
2023-08-03 19:00:20 +0200

    New top navbar background image

--- src/components/navbars/TopNavbar.vue
+++ src/components/navbars/TopNavbar.vue
@@ -1,6 +1,9 @@
 <template>
   <div
-    class="navbar bg-horizontal-navbar-background bg-no-repeat bg-cover bg-center"
+    class="navbar bg-center"
+    :style="{
+      'background-image': getBackgroundImage(),
+    }"
   >
     <div class="navbar-start"></div>
     <div class="navbar-center">
@@ -22,4 +25,15 @@
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { isFactionEvil } from "@/ts/utilities";
+import { usePlayerStore } from "@/stores/playerStores";
+
+const getBackgroundImage = () => {
+  if (isFactionEvil(usePlayerStore().faction)) {
+    return "url(https://ateettea.sirv.com/SusansArt900_fantasy_magical_adult_forest_495096f0-7705-46a0-bad1-73829878d2d2.png)";
+  } else {
+    return "url(https://ateettea.sirv.com/SusansArt900_fantasy_magical_adult_forest_495096f0-7705-46a0-bad1-73829878d2d2.png)";
+  }
+};
+</script>
