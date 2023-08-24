fbbbe6e3114a68551fce28005b1af95052a50e34
Alexandros Stergiou <jutaroroche@gmail.com>
2023-08-24 14:48:33 +0200

    Removed purgeCSS due it causing visual bugs

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,3 @@
-const purgecss = require("@fullhuman/postcss-purgecss");
 const cssnano = require("cssnano");
 
 module.exports = {
@@ -7,10 +6,6 @@ module.exports = {
     require("autoprefixer"),
     cssnano({
       preset: "default",
-    }),
-    purgecss({
-      content: ["./layouts/**/*.html", "./src/**/*.vue", "./src/**/*.jsx"],
-      defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
     }),
   ],
 };
