5dd0ae979a070aeb227bf56987835f843281f62c
Alexandros Stergiou <jutaroroche@gmail.com>
2023-09-12 17:21:31 +0200

    Remade UserDashboardCharacterBasicInfo layout

--- src/views/Dashboards/UserDashboardComponents/UserDashboardCharacterBasicInfo.vue
+++ src/views/Dashboards/UserDashboardComponents/UserDashboardCharacterBasicInfo.vue
@@ -1,20 +1,13 @@
 <template>
-  <div class="bg-base-100 w-full h-16 flex justify-center">
-    <div class="text-center text-neutral-content">
-      <div class="max-w-md">
-        <h1 class="mt-2 text-4xl">{{ ign }}</h1>
-      </div>
-    </div>
-  </div>
-  <div class="flex flex-row justify-evenly">
+  <div class="grid grid-cols-3 justify-items-center">
     <div class="mt-5 shrink">
       <img class="mask mask-circle w-32" :src="avatar" alt="avatar" />
     </div>
     <div
-      class="text-center text-neutral-content bg-base-100 justify-center h-20 w-1/2"
+      class="text-center text-neutral-content justify-center mt-4 h-20 w-1/2"
     >
-      <h1 class="mb-1 text-3xl text-secondary">{{ title }}</h1>
-      <div class="badge badge-outline badge-primary">{{ faction }}</div>
+      <h1 class="mb-1 text-3xl text-neutral-content">{{ characterName }}</h1>
+      <div class="badge badge-outline badge-primary">{{ title }}</div>
     </div>
     <div class="mt-5 w-32 h-64">
       <img :src="factionBanner" alt="faction banner" />
@@ -29,19 +22,22 @@ import { factionNameToBanner } from "@/ts/factionBannersEnum";
 
 const factionBanner = ref<string>("");
 const faction = ref<string>("");
-const ign = ref<string>("");
+const characterName = ref<string>("");
 const title = ref<string>("");
 const avatar = ref<string>(``);
 
 PlayerApiClient.loadPlayerInfo().then((data) => {
   faction.value = data.faction;
   factionBanner.value = factionNameToBanner(data.faction);
-  ign.value = data.ign;
-  avatar.value = `https://mc-heads.net/avatar/${ign.value}/100`;
+  characterName.value = data.ign;
+  avatar.value = `https://mc-heads.net/avatar/${data.ign}/100`;
   PlayerApiClient.loadCharacterInfo().then((data) => {
     title.value = data.title;
     if (title.value === "") {
       title.value = "No Title";
+    }
+    if (data.name) {
+      characterName.value = data.name;
     }
   });
 });
