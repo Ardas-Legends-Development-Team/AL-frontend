a04a93ebfbd49877d98ff92de0a1bef0d6f61e8f
Alexandros Stergiou <jutaroroche@gmail.com>
2023-10-30 21:45:44 +0100

    Changed sizes of top user dashboard bar

--- src/views/Dashboards/UserDashboardComponents/UserDashboardTitles.vue
+++ src/views/Dashboards/UserDashboardComponents/UserDashboardTitles.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="grid grid-cols-3 justify-items-center">
     <div class="mt-5 shrink">
-      <img class="mask mask-circle w-32" :src="avatar" alt="avatar" />
+      <img class="mask mask-circle w-20" :src="avatar" alt="avatar" />
     </div>
     <div
       class="text-center text-neutral-content justify-center mt-4 h-20 w-1/2"
@@ -9,7 +9,7 @@
       <h1 class="mb-1 text-3xl text-neutral-content">{{ characterName }}</h1>
       <div class="badge badge-outline badge-primary">{{ title }}</div>
     </div>
-    <div class="mt-5 w-32 h-64">
+    <div class="my-2 w-16 h-32">
       <img :src="factionBanner" alt="faction banner" />
     </div>
   </div>
