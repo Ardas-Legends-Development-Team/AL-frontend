090e13bbe90559d719c96be35788398666d9cf9f
Alexandros Stergiou <jutaroroche@gmail.com>
2024-01-21 15:26:07 +0100

    Assigned a new variable instead of reassigning claimbuildType variable in getClaimbuildTypeImage functions

--- src/ts/ClaimbuildTypeImageEnum.ts
+++ src/ts/ClaimbuildTypeImageEnum.ts
@@ -1,21 +1,37 @@
 import { isFactionEvil } from "@/ts/utilities";
 
 export enum ClaimbuildTypeImageEnum {
+  // eslint-disable-next-line no-unused-vars
   GOOD_HAMLET = "https://ateettea.sirv.com/Claimbuild%20modal/good%20hamlet.png",
+  // eslint-disable-next-line no-unused-vars
   GOOD_VILLAGE = "https://ateettea.sirv.com/Claimbuild%20modal/good%20village.jpg",
+  // eslint-disable-next-line no-unused-vars
   GOOD_TOWN = "https://ateettea.sirv.com/Claimbuild%20modal/good%20town.png",
+  // eslint-disable-next-line no-unused-vars
   GOOD_CAPITAL = "https://ateettea.sirv.com/Claimbuild%20modal/good%20capital.jpg",
+  // eslint-disable-next-line no-unused-vars
   EVIL_HAMLET = "https://ateettea.sirv.com/Claimbuild%20modal/evil%20hamlet.png",
+  // eslint-disable-next-line no-unused-vars
   EVIL_VILLAGE = "https://ateettea.sirv.com/Claimbuild%20modal/evil%20village.png",
+  // eslint-disable-next-line no-unused-vars
   EVIL_TOWN = "https://ateettea.sirv.com/Claimbuild%20modal/evil%20town.png",
+  // eslint-disable-next-line no-unused-vars
   EVIL_CAPITAL = "https://ateettea.sirv.com/Claimbuild%20modal/evil%20capital.png",
+  // eslint-disable-next-line no-unused-vars
   GOOD_KEEP = "https://ateettea.sirv.com/Claimbuild%20modal/good%20keep.jpg",
+  // eslint-disable-next-line no-unused-vars
   GOOD_CASTLE = "https://ateettea.sirv.com/Claimbuild%20modal/good%20castle.png",
+  // eslint-disable-next-line no-unused-vars
   GOOD_STRONGHOLD = "https://ateettea.sirv.com/Claimbuild%20modal/good%20stronghold.png",
+  // eslint-disable-next-line no-unused-vars
   EVIL_KEEP = "https://ateettea.sirv.com/Claimbuild%20modal/evil%20keep.jpg",
+  // eslint-disable-next-line no-unused-vars
   EVIL_CASTLE = "https://ateettea.sirv.com/Claimbuild%20modal/evil%20castle.png",
+  // eslint-disable-next-line no-unused-vars
   EVIL_STRONGHOLD = "https://ateettea.sirv.com/Claimbuild%20modal/evil%20stronghold.png",
+  // eslint-disable-next-line no-unused-vars
   GOOD_CAMP = "https://ateettea.sirv.com/Claimbuild%20modal/camp.jpg",
+  // eslint-disable-next-line no-unused-vars
   EVIL_CAMP = "https://ateettea.sirv.com/Claimbuild%20modal/camp.jpg",
 }
 
@@ -23,9 +39,9 @@ export function getClaimbuildTypeImage(
   claimbuildType: string,
   factionName: string,
 ): string {
-  claimbuildType = claimbuildType.toLowerCase();
+  const lowercaseClaimbuildType = claimbuildType.toLowerCase();
   const isEvil = isFactionEvil(factionName);
-  switch (claimbuildType) {
+  switch (lowercaseClaimbuildType) {
     case "hamlet":
       return isEvil
         ? ClaimbuildTypeImageEnum.EVIL_HAMLET
@@ -67,9 +83,9 @@ export function getClaimbuildTypeImageAlt(
   claimbuildType: string,
   factionName: string,
 ): string {
-  claimbuildType = claimbuildType.toLowerCase();
+  const lowercaseClaimbuildType = claimbuildType.toLowerCase();
   const isEvil = isFactionEvil(factionName);
-  switch (claimbuildType) {
+  switch (lowercaseClaimbuildType) {
     case "hamlet":
       return isEvil ? "Evil Hamlet" : "Good Hamlet";
     case "village":
