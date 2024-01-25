45ace4366f2924a993f38f2bcb8721cc5acc9d16
GroellerKarim <karim.jonas.groeller@gmail.com>
2024-01-25 21:28:05 +0100

    Add new AuthClient

--- src/ts/ApiService/AuthClient.ts
+++ src/ts/ApiService/AuthClient.ts
@@ -3,9 +3,9 @@ import { ApiClient } from "./ApiClient";
 import { URLSearchParams } from "url";
 
 export class AuthClient extends ApiClient {
-    public authorize(code: string): string {
+    public authorize(code: string, redirectUrl: string): string {
 
-        axios.get(AuthClient.getAuthBaseUrl() + "/authorize?code=" + code)
+        axios.get(AuthClient.getAuthBaseUrl() + "/authorize?code=" + code + "&redirectUrl=" + redirectUrl)
 
         return ""
     }
